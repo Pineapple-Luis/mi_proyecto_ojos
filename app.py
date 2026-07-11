15172d195cbd9c798ec26f02bc8e5cd5e11564b4
Luis Enrique López Zamora <luislo1994@hotmail.com>
2026-07-11 09:31:20 -0600

    Set debug mode to False in app.run
    Disable debug mode when running the app.

--- app.py
+++ app.py
@@ -125,5 +125,6 @@ def predict():
 # 5. LANZAMIENTO
 # ============================================
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
