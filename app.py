75e05907a7169109a6e5ba0be7cb0f6ee2595e26
Luis Enrique López Zamora <luislo1994@hotmail.com>
2026-07-11 09:42:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,7 @@ def predict():
 # 5. LANZAMIENTO
 # ============================================
 if __name__ == '__main__':
+    from waitress import serve
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    serve(app, host='0.0.0.0', port=port)
